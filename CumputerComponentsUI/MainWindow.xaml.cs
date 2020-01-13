dc80e97f99c0eb50482e91e4a9a7e7c434691471
Mykhailo Horodnyi <mhorodnyi@outlook.com>
2020-01-13 19:19:42 +0200

    Refactored events and simplified code

--- CumputerComponentsUI/MainWindow.xaml.cs
+++ CumputerComponentsUI/MainWindow.xaml.cs
@@ -12,9 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ComputerComponents.Models;
 using CumputerComponentsUI.ViewModel;
-using CumputerComponentsUI.Views.Windows;
 
 namespace CumputerComponentsUI
 {
@@ -27,85 +25,10 @@ namespace CumputerComponentsUI
         {
             InitializeComponent();
         }
-
-        private void MotherboardView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        private void ChooseComponent(object sender, MouseButtonEventArgs e)
         {
-            MotherboardsList motherboards = new MotherboardsList();
-            motherboards.Show();
-            motherboards.Closed += (object sender1, EventArgs ev) =>
-            {
-                MotherboardView.DataContext = Assembly.Motherboard;
-            };
-        }
-
-        private void CPUView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            CPUList cpus = new CPUList();
-            cpus.Show();
-            cpus.Closed += (object sender1, EventArgs ev) =>
-            {
-                CPUView.DataContext = Assembly.CPU;
-            };
-        }
-
-        private void GPUView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            GPUList gpus = new GPUList();
-            gpus.Show();
-            gpus.Closed += (object sender1, EventArgs ev) =>
-            {
-                GPUView.DataContext = Assembly.GPU;
-            };
-        }
-
-        private void MemoryView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            MemoryList memories = new MemoryList();
-            memories.Show();
-            memories.Closed += (object sender1, EventArgs ev) =>
-            {
-                MemoryView.DataContext = Assembly.Memory;
-            };
-        }
-
-        private void PowerSupplyView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            PowerSupplyList PSs = new PowerSupplyList();
-            PSs.Show();
-            PSs.Closed += (object sender1, EventArgs ev) =>
-            {
-                PowerSupplyView.DataContext = Assembly.PowerSupply;
-            };
-        }
-
-        private void FanView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            FanList fans = new FanList();
-            fans.Show();
-            fans.Closed += (object sender1, EventArgs ev) =>
-            {
-                FanView.DataContext = Assembly.Fan;
-            };
-        }
-
-        private void HDDView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            HDDList hdds = new HDDList();
-            hdds.Show();
-            hdds.Closed += (object sender1, EventArgs ev) =>
-            {
-                HDDView.DataContext = Assembly.HDD;
-            };
-        }
-
-        private void SSDView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            SSDList ssds = new SSDList();
-            ssds.Show();
-            ssds.Closed += (object sender1, EventArgs ev) =>
-            {
-                SSDView.DataContext = Assembly.SSD;
-            };
+            EventsHelper.GetDataContext(sender);
+            EventsHelper.GetWindow(sender);
         }
     }
 }
---
+++ CumputerComponentsUI/ViewModel/EventsHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ComputerComponents.Interfaces;
+using ModelsComponents = ComputerComponents.Models;
+using ViewComponents = CumputerComponentsUI.Views.Components;
+using ComponentWindow = CumputerComponentsUI.Views.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CumputerComponentsUI.ViewModel
+{
+    static class EventsHelper
+    {
+        private static IComponent LastComponent;
+        private static Window LastWindow;
+
+        public static void GetWindow(object uc)
+        {
+            LastWindow.Show();
+            LastWindow.Closed += (object sender, EventArgs e) =>
+            {
+                GetDataContext(uc);
+                ((UserControl)uc).DataContext = EventsHelper.LastComponent;
+            };
+        }
+        public static void GetDataContext(object obj)
+        {
+            if (obj is ViewComponents.Motherboard)
+            {
+                LastComponent = Assembly.Motherboard;
+                LastWindow = new ComponentWindow.MotherboardsList();
+            }
+            else if (obj is ViewComponents.CPU)
+            {
+                LastComponent = Assembly.CPU;
+                LastWindow = new ComponentWindow.CPUList();
+            }
+            else if (obj is ViewComponents.GPU)
+            {
+                LastComponent = Assembly.GPU;
+                LastWindow = new ComponentWindow.GPUList();
+            }
+            else if (obj is ViewComponents.Memory)
+            {
+                LastComponent = Assembly.Memory;
+                LastWindow = new ComponentWindow.MemoryList();
+            }
+            else if (obj is ViewComponents.PowerSupply)
+            {
+                LastComponent = Assembly.PowerSupply;
+                LastWindow = new ComponentWindow.PowerSupplyList();
+            }
+            else if (obj is ViewComponents.Fan)
+            {
+                LastComponent = Assembly.Fan;
+                LastWindow = new ComponentWindow.FanList();
+            }
+            else if (obj is ViewComponents.HDD)
+            {
+                LastComponent = Assembly.HDD;
+                LastWindow = new ComponentWindow.HDDList();
+            }
+            else if (obj is ViewComponents.SSD)
+            {
+                LastComponent = Assembly.SSD;
+                LastWindow = new ComponentWindow.SSDList();
+            }
+        }
+    }
+}
